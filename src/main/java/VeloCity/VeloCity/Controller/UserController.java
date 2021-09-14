96a660e4d3100aa94b5874d9603b1e410e180695
NazirShaikh <shain003.212@gmail.com>
2021-09-14 14:14:30 +0100

    messed around with dependencies

--- src/main/java/VeloCity/VeloCity/Controller/UserController.java
+++ src/main/java/VeloCity/VeloCity/Controller/UserController.java
@@ -1,4 +1,11 @@
 package VeloCity.VeloCity.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
 public class UserController {
+    @GetMapping("")
+
 }
