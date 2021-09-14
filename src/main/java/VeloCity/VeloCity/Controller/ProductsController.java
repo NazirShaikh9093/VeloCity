2ff312a4ce261d14f33e15307852d589001c8f1f
NazirShaikh <shain003.212@gmail.com>
2021-09-14 15:19:28 +0100

    finally can see our work

--- src/main/java/VeloCity/VeloCity/Controller/ProductsController.java
+++ src/main/java/VeloCity/VeloCity/Controller/ProductsController.java
@@ -1,6 +1,13 @@
 package VeloCity.VeloCity.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
 @Controller
 public class ProductsController {
-    @GetMapping("")
+    @GetMapping("/products")
+    public String products() {
+        return "/products";
+    }
 }
