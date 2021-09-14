e946168b24317cc533f3654656af43a1ba1f6cc8
NazirShaikh <shain003.212@gmail.com>
2021-09-14 11:11:14 +0100

    first commit

---
+++ src/main/java/VeloCity/VeloCity/VeloCityApplication.java
@@ -0,0 +1,13 @@
+package VeloCity.VeloCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeloCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeloCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/VeloCity/VeloCity/VeloCityApplicationTests.java
@@ -0,0 +1,13 @@
+package VeloCity.VeloCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeloCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
