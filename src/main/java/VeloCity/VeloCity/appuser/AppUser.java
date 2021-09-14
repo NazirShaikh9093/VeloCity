ad03c886a187b6433c7afb878b5d6bfc98fc8159
AlexiaConstant <alexia.constant@yahoo.com>
2021-09-14 16:43:49 +0100

    App user authority and database set up

---
+++ src/main/java/VeloCity/VeloCity/appuser/AppUser.java
@@ -0,0 +1,56 @@
+package VeloCity.VeloCity.appuser;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class AppUser implements UserDetails {
+
+    private Long id;
+    private  String name;
+    private String username;
+    private String email;
+    private String password;
+    private AppUserRole AppUserRole;
+    private Boolean Locked;
+    private Boolean enabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(AppUserRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !Locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
