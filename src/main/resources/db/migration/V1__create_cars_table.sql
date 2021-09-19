f7ef8ad04e82eda0b7fc5a8783d2e2e1ee69c3c2
NazirShaikh <shain003.212@gmail.com>
2021-09-19 18:49:50 +0100

    added SQL files for 4 cars, will add the other 8 later

--- src/main/resources/db/migration/V1__create_cars_table.sql
+++ src/main/resources/db/migration/V1__create_cars_table.sql
@@ -12,13 +12,3 @@ CREATE TABLE cars (
     image4 varchar(300) NOT NULL,
     available boolean NOT NULL
 );
-
-INSERT INTO cars
-(model, price, transmission, seats, quantity, description, image1, image2, image3, image4, available)
-VALUES ('BMW i3', '16000', 'Automatic/Manual', '4', '5',
-'The latest Vauxhall Corsa is miles better than the car it replaced, offering a much more convincing blend of performance, economy, comfort and driving pleasure. It looks good, boasts one of the best petrol engines in its class and has benefitted hugely from the thoroughly modern underpinnings shared with the latest Peugeot 208. All of the technology on-board is bang up-to-date too, but we can’t help feeling the whole package is a little overpriced; key rivals like the Ford Fiesta and Renault Clio are similarly rounded, but better to drive and cheaper to buy.',
-'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+1.jpeg',
-'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+2.jpeg',
-'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+3.jpeg',
-'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+4.jpeg',
-'TRUE');
