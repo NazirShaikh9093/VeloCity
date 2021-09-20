edb66edbe837f068f6f66671b5aaeae8f55d4bdd
NazirShaikh <shain003.212@gmail.com>
2021-09-20 15:25:01 +0100

    fixed sql files, removed double quotation marks as it gets upset over them

--- src/main/resources/db/migration/V2__BMW_i3.sql
+++ src/main/resources/db/migration/V2__BMW_i3.sql
@@ -1,7 +1,7 @@
 INSERT INTO cars
 (model, price, transmission, seats, quantity, description, image1, image2, image3, image4, available)
 VALUES ('BMW i3', '60', 'Manual', '4', '5',
-"Ever since it was first unveiled, the BMW i3 has played a major role in bringing electric cars into the mainstream. With its funky looks, impressive performance and clever carbonfibre and aluminium construction, the i3 has shown buyers that electric cars don't have to be white goods – they can be lusted after."",
+'Ever since it was first unveiled, the BMW i3 has played a major role in bringing electric cars into the mainstream. With its funky looks, impressive performance and clever carbonfibre and aluminium construction, the i3 has shown buyers that electric cars dont have to be white goods – they can be lusted after.',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+1.jpeg',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+2.jpeg',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/BMW+i3/BMW+i3+image+3.jpeg',
