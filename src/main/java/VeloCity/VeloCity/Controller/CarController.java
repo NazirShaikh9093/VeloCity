2817c72d29d7c4424ff15003fe47e89dee9b3610
NazirShaikh <shain003.212@gmail.com>
2021-09-21 14:50:12 +0100

    checkpoint with trying to load a car page with info on a selected car

--- src/main/java/VeloCity/VeloCity/Controller/CarController.java
+++ src/main/java/VeloCity/VeloCity/Controller/CarController.java
@@ -1,14 +1,24 @@
 package VeloCity.VeloCity.Controller;
 
 
+import VeloCity.VeloCity.Model.Car;
+import VeloCity.VeloCity.Repository.CarRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class CarController {
-    @GetMapping("/car")
-    public String car() {
+
+    CarRepository carRepository;
+
+    @GetMapping("/car/{id}")
+    public String car(@RequestParam int id, Model model) {
+        Optional<Car> car = carRepository.findById((long) id);
+        model.addAttribute("car", car);
         return "/car";
     }
 }
