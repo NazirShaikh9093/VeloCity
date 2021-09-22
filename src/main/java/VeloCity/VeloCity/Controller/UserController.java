54fbb9780e0fc706c225b689b00c3b6203045d9d
toluba <tomikeolubamowo@aol.com>
2021-09-22 12:34:50 +0100

    sign in done

--- src/main/java/VeloCity/VeloCity/Controller/UserController.java
+++ src/main/java/VeloCity/VeloCity/Controller/UserController.java
@@ -1,4 +1,18 @@
 package VeloCity.VeloCity.Controller;
 
+import VeloCity.VeloCity.Model.Car;
+import VeloCity.VeloCity.Repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class UserController {
+    @GetMapping("/signin")
+    public String signin() {
+        return "/signin";
+    }
 }
