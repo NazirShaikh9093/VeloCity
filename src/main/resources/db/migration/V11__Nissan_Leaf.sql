da66fa497add00ed07b59bbe07ae8d50a8a54879
NazirShaikh <shain003.212@gmail.com>
2021-09-22 12:32:08 +0100

    cut description size for cars because it was too long and varied on the website when it loaded

--- src/main/resources/db/migration/V11__Nissan_Leaf.sql
+++ src/main/resources/db/migration/V11__Nissan_Leaf.sql
@@ -1,7 +1,7 @@
 INSERT INTO cars
 (model, price, transmission, seats, quantity, description, image1, image2, image3, image4, available)
 VALUES ('Nissan Leaf', '170', 'Automatic', '5', '2',
-'The Nissan Leaf impresses in pretty much every area. It’s easy to drive and comfortable, especially around town, and it has a decent range that should make it appealing to a wide range of buyers. Given how much it costs, there’s an impressive amount of tech on offer, too, and it has enough space for all the family. There’s a chance that people who already own one of the first-generation Leafs will be unhappy to see just how ‘normal’ this model makes owning, driving and living with an electric car – in its looks, interior, interface and the way it drives. However, for us, our only real disappointment is that, from a quality and style point of view, it’s still not as appealing as the new VW ID.3.',
+'The Nissan Leaf impresses in pretty much every area. It’s easy to drive and comfortable, especially around town, and it has a decent range that should make it appealing to a wide range of buyers. Given how much it costs, there’s an impressive amount of tech on offer, too, and it has enough space for all the family.',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/Nissan+Leaf/Nissan+Leaf+image+1.jpeg',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/Nissan+Leaf/Nissan+Leaf+image+2.jpeg',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/Nissan+Leaf/Nissan+Leaf+image+3.jpeg',
--- src/main/resources/db/migration/V5__Ford_Mondeo.sql
+++ src/main/resources/db/migration/V5__Ford_Mondeo.sql
@@ -1,7 +1,7 @@
 INSERT INTO cars
 (model, price, transmission, seats, quantity, description, image1, image2, image3, image4, available)
 VALUES ('Ford Mondeo', '150', 'Automatic', '5', '10',
-'The Ford Mondeo - now in its fourth generation - is the largest and priciest it has ever been, although the family car is also a more premium, refined and high-tech offering than it was before. It faces fierce competition from the likes of the Skoda Superb, Mazda 6 and VW Passat, but its core strengths ensure that it holds its own. The Mondeo is available as either a hatchback or an estate, and both body styles have an abundance of space for passengers and luggage, making them very practical for families. The engine line-up is not blessed with performance, it has to be said, but the diesels are very smooth and efficient, and offer enough power to get by on the road.',
+'The Ford Mondeo - now in its fourth generation - is the largest and priciest it has ever been, although the family car is also a more premium, refined and high-tech offering than it was before. It faces fierce competition from the likes of the Skoda Superb, Mazda 6 and VW Passat, but its core strengths ensure that it holds its own.',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/Ford+Mondeo/Ford+Mondeo+image+1.jpeg',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/Ford+Mondeo/Ford+Mondeo+image+2.jpeg',
 'https://rentcarswebsite.s3.eu-west-2.amazonaws.com/Car+images/Ford+Mondeo/Ford+Mondeo+image+3.jpeg',
